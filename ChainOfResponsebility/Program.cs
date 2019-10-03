b7b879581f7532b6694cb48905fb51b734a59a94
Иван Хомичков <ivan.khomichkov@gmail.com>
2019-10-03 21:57:44 +0300

    chain of resposibility

---
+++ ChainOfResponsebility/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChainOfResponsebility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
